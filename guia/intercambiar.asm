f3033910d4e782b8c4ca844decf0b7321c80d1a7
raquelwek <radavila@fi.uba.ar>
2024-11-04 15:38:14 +0000

    el bagre no se rinde

--- guia/intercambiar.asm
+++ guia/intercambiar.asm
@@ -6,9 +6,12 @@ global intercambiar
 
 ;Dado un string, devuelve un string in-place con los caracteres en orden inverso.
 ;void intercambiar(char* str);
-;   for i=0; i < len(str)//2;i++:
-;       j = len(str)-1-i
+;   i = 0
+;   j = strlen(str) - 1
+;   while i < j:
 ;       str[i], str[j] = str[j], str[i]
+;       i++
+;       j--
 
 intercambiar:
     push rbp
@@ -16,30 +19,32 @@ intercambiar:
 
     push r12
     push r13
-    mov r12, rdi    ;almaceno puntero al primer char
-    
-    call strlen      ;guarda len en rax
-    mov r13, rax     ;r13 = strlen(c)
+    mov r12, rdi    ; almaceno puntero al primer char
 
-    mov r11, rax
-    shr r11, 1      ;r11 = len(arr)//2
-    dec r13         ;r13 = len(arr)-1
-    xor r8, r8      ;usar r8 como contador
+    call strlen      ; guarda len en rax
+    mov r13, rax     ; j = strlen(str)
 
-    .loop:
-        mov rax, r13
-        sub rax, r8
-        mov r9b, BYTE[r12+rax]  ;str[j] en r9b
-        mov r10b, BYTE[r12+r8]  ;str[i] en r10b
-        mov BYTE[r12+r8], r9b ;str[i] = str[j]
-        mov BYTE[r12+rax], r10b  ;str[j] = str[i]
-        inc r8
-        cmp r8, r11
-        jl .loop
-    
+    cmp r13, 0
+    je .fin
+    dec r13          ; j = j - 1
+    xor r8, r8       ; usar r8 como contador i
+
+.loop:
+    mov r9b, byte [r12 + r13]  ; str[j] en r9b
+    mov r10b, byte [r12 + r8]  ; str[i] en r10b
+    mov [r12 + r8], r9b        ; str[i] = str[j]
+    mov [r12 + r13], r10b      ; str[j] = str[i]
+    inc r8
+    dec r13
+    cmp r8, r13
+    jb .loop                    ; continuar si i < j
+
+.fin:
     pop r13
     pop r12
     pop rbp
+    ret
+
 
 strLen:
     push rbp
